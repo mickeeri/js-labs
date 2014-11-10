d41f54d22208ad063746a41cae8aa6095cad829e
me222wm <me222wm@student.lnu.se>
2014-11-10 12:30:37 +0100

    Returns max, min and average age.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -4,11 +4,12 @@ var makePerson = function(persArr){
 
 var persons = {}; 
 var nameArray = []; 
+var ageArray = []; 
 
 // Skapar en ny array för bara namn. 
 persArr.forEach(function (array, index){
 	nameArray[index] = array.name; 
-})
+}); 
 
 // Sorterar den nya arrayen. 
 nameArray.sort(); 
@@ -20,9 +21,21 @@ persons.names = nameArray.reduce(function (a, b){
 
 // Skapar en array med bara nummer. 
 persArr.forEach(function (array, index){
-	
-})
+	ageArray[index] = array.age; 
+}); 
 
+// Sorterar. 
+ageArray.sort(); 
+
+persons.minAge = ageArray[0]; 
+persons.maxAge = ageArray[ageArray.length - 1]; 
+
+// Räknar ut medelvärdet. 
+var sum = ageArray.reduce(function (a, b){
+	return a + b; 
+}); 
+
+persons.averageAge = Math.round(sum / ageArray.length); 
 
 return persons; 
 
