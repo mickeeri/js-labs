8fc68a91ccfdd4ed54d0569d3281e289158640f0
me222wm <me222wm@student.lnu.se>
2014-11-11 15:40:16 +0100

    Added error handling.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -6,40 +6,53 @@ var persons = {};
 var nameArray = []; 
 var ageArray = []; 
 
-// Skapar en ny array för bara namn. 
-persArr.forEach(function (array, index){
-	nameArray[index] = array.name; 
-}); 
+try{
+	// Skapar en ny array för bara namn. 
+	persArr.forEach(function (array, index){
+		nameArray[index] = array.name; 
+		
+		if(typeof array.name !== "string"){
+			throw new Error("Fel! ("+ array.name + ") är inte en textsträng.");
+		}
+	}); 
 
-// Sorterar den nya arrayen. Använder String.localeCompare för att även inkludera svenska tecken. 
-nameArray = nameArray.sort(function (a, b){
-	return a.localeCompare(b); 
-}); 
+	// Sorterar den nya arrayen. Använder String.localeCompare för att även inkludera svenska tecken. 
+	nameArray = nameArray.sort(function (a, b){
+		return a.localeCompare(b); 
+	}); 
 
-// Adderar dem till en sträng och lägger till i objektet. 
-persons.names = nameArray.reduce(function (a, b){
-	return a + ", " + b; 
-}); 
+	// Adderar dem till en sträng och lägger till i objektet. 
+	persons.names = nameArray.reduce(function (a, b){
+		return a + ", " + b; 
+	}); 
 
-// Skapar en array med bara nummer. 
-persArr.forEach(function (array, index){
-	ageArray[index] = array.age; 
-}); 
+	// Skapar en array med bara nummer. 
+	persArr.forEach(function (array, index){
+		ageArray[index] = array.age; 
 
-// Sorterar. 
-ageArray.sort(); 
+		// if(typeof array.age !== "number"){
+		// 	throw new Error("Fel! (" + array.age + ") är inte ett heltal.");
+		// }
+	}); 
 
-persons.minAge = ageArray[0]; 
-persons.maxAge = ageArray[ageArray.length - 1]; 
+	// Sorterar. 
+	ageArray.sort(); 
 
-// Räknar ut medelvärdet. 
-var sum = ageArray.reduce(function (a, b){
-	return a + b; 
-}); 
+	persons.minAge = ageArray[0]; 
+	persons.maxAge = ageArray[ageArray.length - 1]; 
 
-persons.averageAge = Math.round(sum / ageArray.length); 
+	// Räknar ut medelvärdet. 
+	var sum = ageArray.reduce(function (a, b){
+		return a + b; 
+	}); 
 
-return persons; 
+	persons.averageAge = Math.round(sum / ageArray.length); 
+
+	return persons; 
+}
+catch(error){
+	alert(error.message); 
+}
 
 };
 
