f7bb53d5739dab8a4352ca40a92d1cd9f9a6995c
me222wm <me222wm@student.lnu.se>
2014-11-20 15:20:06 +0100

    Started to query and create new messages.

--- 2-labbymezzage/js/MessageBoard.js
+++ 2-labbymezzage/js/MessageBoard.js
@@ -2,9 +2,27 @@
 
 var MessageBoard = {
 
-	// messages: []; 
+	// var messages: []; 
+
+
+
 
 	init:function(e){
+		var newMessage = document.querySelector("#messages p")
+		var input = document.querySelector("#text"); 
+		var submit = document.querySelector("#send");  
+		
+		submit.addEventListener("click", function(e){
+		e.preventDefault(); // Hindra att formuläret skickas till servern. 
+
+		newMessage.classList.remove("error");
+
+		var mess = new Message(input.value, new Date());
+
+		newMessage.innerHTML = mess; 
+	}); 
+
+
 		// var mess = new Message("Testmeddelande", new Date()); 
 		// alert(mess); 
 		// alert(mess.getText()); 
