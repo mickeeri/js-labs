414f7be39a5678084d76bd1cd759d0798e90233b
me222wm <me222wm@student.lnu.se>
2014-11-30 15:59:46 +0100

    Now generates table.

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,16 +1,44 @@
-var pictureTable = function(randomArray){
+"use strict";
+ 
+ var rows = 4; 
+ var columns = 5; 
 
 
-}; 
+var pictureTable = function(){
 
+// console.log(randomArray); 
 
+	var table = document.getElementById("memorytable"); 
+
+	// Rows
+	for(var i = 0; i < rows; i++){
+		createTable(); 
+		console.log(table); 
+	}
+
+	// Columns
+	function createTable(){
+	 	var tr = document.createElement("tr");
+		table.appendChild(tr); 
+		
+		for(var j = 0; j < columns; j++){
+			var td = document.createElement("td"); 
+		 	
+		 	var a = document.createElement("a"); 
+				a.href = "#"; 
+				a.innerHTML = "BILD"; 
+
+			tr.appendChild(td); 
+			td.appendChild(a); 
+		}
+	} 
+} 
+
+// var rows = 3; 
+// var columns = 4; 
 
 window.onload = function(){
 
-	var randomArray = RandomGenerator.getPictureArray(3,4); 
-
-	console.log(randomArray); 
-
-	pictureTable(randomArray); 
+	pictureTable(); 
 
 };
