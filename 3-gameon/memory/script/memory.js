a971384be340910f4620687222ebe3be00d59e1d
me222wm <me222wm@student.lnu.se>
2014-11-30 17:24:55 +0100

    Added functions. Now able to display random img on click.

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,38 +1,76 @@
 "use strict";
  
- var rows = 4; 
- var columns = 5; 
+var rows = 4; 
+var columns = 4; 
+var idCount = 0; 
+
 
 
 var pictureTable = function(){
 
-// console.log(randomArray); 
+	 var randomArray = RandomGenerator.getPictureArray(rows, columns); 
+
+	// var newArray = []; 
+	// newArray.push(randomArray); 
+	// console.log(randomArray); 
+
+	// console.log(newArray); 
+
+	// console.log(randomArray); 
 
 	var table = document.getElementById("memorytable"); 
 
 	// Rows
 	for(var i = 0; i < rows; i++){
-		createTable(); 
-		console.log(table); 
+		createTableColumns(); 
 	}
 
 	// Columns
-	function createTable(){
+	function createTableColumns(){
 	 	var tr = document.createElement("tr");
 		table.appendChild(tr); 
 		
 		for(var j = 0; j < columns; j++){
+			
 			var td = document.createElement("td"); 
-		 	
-		 	var a = document.createElement("a"); 
-				a.href = "#"; 
-				a.innerHTML = "BILD"; 
-
+			// td.setAttribute("id", randomArray[idCount]); 
+		 
 			tr.appendChild(td); 
-			td.appendChild(a); 
+
+			createImageLink(td); 
+
+			// idCount++; 
 		}
+	}
+
+	function createImageLink(td){
+
+
+		var a = document.createElement("a"); 
+		a.href = "#"; 
+		var defaultImg = document.createElement("img"); 
+		defaultImg.src = "pics/0.png"; 
+		a.id = idCount; 
+
+		++idCount;
+
+
+		td.appendChild(a); 
+		a.appendChild(defaultImg); 
+
+		a.onclick = function(){
+			flipCard(a, defaultImg); 
+		};
 	} 
-} 
+
+	function flipCard(a, image){
+		
+		image.src = "pics/" + randomArray[a.getAttribute("id")] + ".png"; 
+		console.log(a); 
+
+	}
+
+}; 
 
 // var rows = 3; 
 // var columns = 4; 
