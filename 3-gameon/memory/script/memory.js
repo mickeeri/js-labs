bbc4f2152ffa3677e077b747805970df0e72b048
me222wm <me222wm@student.lnu.se>
2014-12-05 10:05:50 +0100

    Improved application.

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,32 +1,36 @@
 "use strict";
 
-var MemoryApp = function(rows, columns, gameId){
+var MemoryApp = function(rows, columns, gameId) {
 
-	var doc = {
-		div: document.getElementById(gameId),
-		randomPictureArray: new RandomGenerator.getPictureArray(rows, columns),
-		idCount: 0,
-		pictures: [],
-		numberOfTries: 0,
-		numberOfPairs: (rows * columns)/2,
-		counter: document.createElement("p")	
-	};
+    var doc = {
+        div: document.getElementById("memory"),
+        game: document.createElement("div"),
+        randomPictureArray: new RandomGenerator.getPictureArray(rows, columns),
+        idCount: 0,
+        pictures: [],
+        numberOfTries: 0,
+        numberOfPairs: (rows * columns) / 2,
+        counter: document.createElement("p")
+    };
 
+	doc.div.appendChild(doc.game);
+	doc.game.id = gameId;
+	doc.game.setAttribute("class", "games"); 
 	doc.counter.innerHTML = "Antal försök: " + doc.numberOfTries; 
-	doc.div.appendChild(doc.counter); 
+	doc.game.appendChild(doc.counter); 
 	
 	CreateTable();  
 
-	function CreateTable(){
+	function CreateTable() {
 
-		var table = document.createElement("table"); 
-		doc.div.appendChild(table); 		
+	    var table = document.createElement("table");
+		doc.game.appendChild(table); 		
 
-		for(var i = 0; i < rows; i++){
+		for(var i = 0; i < rows; i++) {
 			var tr = document.createElement("tr");
 			table.appendChild(tr); 
 
-			for(var j = 0; j < columns; j++){
+			for(var j = 0; j < columns; j++) {
 				var td = document.createElement("td");
 				tr.appendChild(td); 
 				CreatePictureLinks(td);
@@ -62,73 +66,74 @@ var MemoryApp = function(rows, columns, gameId){
 				return false; 
 			}
 
-			if(doc.pictures.length < 2){
-				// Assign image id by array index and a-tag id.  
+            // The card can only flip if the length of the array is smaller or equals two. 
+			if (doc.pictures.length <= 2) {
+
+			    // Assign image id by array index and a-tag id.  
 				picture.src = "pics/" + doc.randomPictureArray[card.getAttribute("id")] + ".png";
 			
-				// To seperate and be able to compare two pictures I use an array. The array will never contain more than two pictures. 
-				if(doc.pictures.length === 0){
-					doc.pictures[0] = picture; 
-				}
-				// Checks if the user clicks the same link by comparing a-tag id. 
-				if(e.currentTarget !== doc.pictures[0].parentNode){
-					doc.pictures[1] = picture; 
+				doc.pictures.push(picture);
+			
+				if (doc.pictures.length === 2) {
+
+				    doc.numberOfTries += 1;
+
+				    doc.counter.innerHTML = "Antal försök: " + doc.numberOfTries;
+
+				    EvaluatePair();
 				}
 			}
-			console.log(doc.pictures);
-
-			// Lägga in detta i egen funktion?
-			if(doc.pictures.length === 2){
-
-				doc.numberOfTries += 1; 
-
-				doc.counter.innerHTML = "Antal försök: " + doc.numberOfTries; 
-
-				if(doc.pictures[0].src !== doc.pictures[1].src){
-					
-					setTimeout(function(){
-						// Resets pics to default img.
-						doc.pictures[0].src = "pics/0.png"; 
-						doc.pictures[1].src = "pics/0.png"; 
-						doc.pictures = []; 
-					}, 800); 
-				}
-				else{
-					console.log(doc.pictures[1].parentNode); 
-					doc.pictures = []; 
-					doc.numberOfPairs -= 1; 
-
-					if(doc.numberOfPairs === 0){
-						 PresentResult();
-					}
-				}
-
-			}	
-
 		});		
 	}
+	function EvaluatePair() {
+        // If the pictures match. 
+	    if (doc.pictures[0].src !== doc.pictures[1].src) {
+
+	        setTimeout(function () {
+	            doc.pictures[0].src = "pics/0.png";
+	            doc.pictures[1].src = "pics/0.png";
+	            doc.pictures.splice(0, 2);
+	        }, 800);
+
+	    }
+	    else{
+	        
+	        doc.numberOfPairs -= 1;
+	        doc.pictures.splice(0, 2);
+
+	        if (doc.numberOfPairs === 0) {
+	            PresentResult();
+	        }
+	    }
+	}
+
 	function PresentResult(){
 		var result = document.createElement("p");
 		result.innerHTML = "Grattis! Spelet är slut. Du behövde " + doc.numberOfTries + " försök för att lyckas.";
 
-		doc.div.appendChild(result); 
+		doc.game.appendChild(result); 
 
-		var playAgain = document.createElement("input"); 
-		playAgain.type = "button"; 
-		playAgain.value = "Spela igen"; 
-		doc.div.appendChild(playAgain); 
+		//var playAgain = document.createElement("input"); 
+		//playAgain.type = "button"; 
+		//playAgain.value = "Spela igen"; 
+		//doc.game.appendChild(playAgain); 
 
-		playAgain.onclick = function(){
-			doc.div.innerHTML = "";
-			new MemoryApp(rows, columns, doc.div.id);
-		}; 
+		//playAgain.onclick = function(){
+
+		//    doc.div.removeChild(doc.game); 
+		//    // doc.game.innerHTML = "";
+		//	new MemoryApp(rows, columns, doc.game.id);
+		//}; 
 		
 	}
 };
 
 window.onload = function(){
 
-	new MemoryApp(2, 3, "game1"); 
-	new MemoryApp(4, 4, "game2"); 
+	MemoryApp(2, 3, "game1"); 
+	MemoryApp(4, 4, "game2");
+	MemoryApp(2, 3, "game3");
+	MemoryApp(1, 4, "game4");
+	MemoryApp(2, 2, "game5"); 
 
 };
