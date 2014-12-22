aedeb0e7f170eab125bcbb183a0718fbc8fa1a7f
me222wm <me222wm@student.lnu.se>
2014-12-22 12:25:16 +0100

    push.

--- 4-thequiz/js/quiz.js
+++ 4-thequiz/js/quiz.js
@@ -1,4 +1,4 @@
-//"use strict"; 
+"use strict"; 
 
 window.onload = function(){
 	
@@ -6,115 +6,126 @@ window.onload = function(){
 	var div = document.getElementById("content");
 	var url = "http://vhost3.lnu.se:20080/question/1"; 
 	var pQuestion = document.getElementById("question"); 
-	var pFeedback = document.createElement("p");
+	var pFeedback = document.getElementById("feedback");
 	var inputAnswer = document.getElementById("answer"); 
 	var sendButton = document.getElementById("send"); 
+	var H3questionNumber = document.getElementById("questionnumber"); 
 	var numberOfWrongAnswers = 0; 
-
-
-	div.appendChild(pFeedback);
+	var guessArray = []; 
+	var questionNumber = 1; 
+	var resultObject; 
+	var counter = 0; 
 
 	getQuestion(); 
 
 	function getQuestion(){
 
+		counter = 0; 
+		
 		inputAnswer.focus(); 
+		H3questionNumber.innerHTML = "Fråga " + questionNumber; 
 
 		var xhr = new XMLHttpRequest(); 
-
 		xhr.onreadystatechange = function(){
 		
 			if (xhr.readyState === 4 && xhr.status === 200) { 
 
 				object = JSON.parse(xhr.responseText); // Nu får jag objekt istället. 
-
 				pQuestion.innerHTML = object.question; 
 			}
 		}; 
 
 		xhr.open("get", url, true); 
-
 		xhr.send(null); 
 
-		// inputAnswer.focus(); 
+		sendButton.addEventListener("click", handler);
+		inputAnswer.addEventListener("keypress", handler); 
 
-		sendButton.addEventListener("click", function(){
-			sendAnswer(); 
-			// http://www.sitepoint.com/create-one-time-events-javascript/
-			e.target.removeEventListener(e.type, arguments.callee);
-		}); 
+		function handler(e){			
+			if(e.button === 0 || e.keyCode === 13){
+				
+		   		counter += 1; 
 
-		inputAnswer.addEventListener("keypress", function(e){
-			if(e.keyCode === 13){
-				sendAnswer(); 
-				e.target.removeEventListener(e.type, arguments.callee);
+		   		if (counter === 1){
+		   			sendAnswer(); 
+		   		}
+
+		   		// To prevent event from launching more than one time. http://www.sitepoint.com/create-one-time-events-javascript/
+		   		// e.target.removeEventListener(e.type, arguments.callee);
 			}
-		}); 
+		}
 	}
 
 	function sendAnswer(){
-		
+
 		var xhrAnswer = new XMLHttpRequest(); 
 			
+		// JSON object that gets sendt containing users input. 
 		var answerObject = {"answer": document.getElementById("answer").value};
 
 		xhrAnswer.onreadystatechange = function(){
 
 			if (xhrAnswer.readyState === 4) {
 
-				
-				console.log(answerObject);  
-
-
 				object = JSON.parse(xhrAnswer.responseText); 
 
-
-				console.log(object.message); 
-
 				if(object.message === "Correct answer!" && xhrAnswer.status === 200){
+
 					inputAnswer.value = ""; 
-					pFeedback.setAttribute("style", "color: black;"); 
+					pFeedback.setAttribute("style", "color: black;"); 	
+					resultObject = {"questionnr": questionNumber, "numberofwronganswers": numberOfWrongAnswers}; 
+					questionNumber += 1; 
+					guessArray.push(resultObject); 
 					numberOfWrongAnswers = 0; 
 					pFeedback.innerHTML = "Rätt svar";
 
 					url = object.nextURL; 
 					
 					if(url !== undefined){		
-						getQuestion(); 
+						getQuestion(); 						
 					}
+					// Return contains no URL = no more questions. 
 					else{
-
+						pQuestion.innerHTML = ""; 
 						pFeedback.innerHTML = "Grattis! Slut på frågor.";
 						sendButton.disabled = true; 
 						inputAnswer.disabled = true; 
+						H3questionNumber.innerHTML="";
+
+						var resultH = document.createElement("h2"); 
+						div.appendChild(resultH); 
+						resultH.innerHTML = "Resultat"; 
+						var resultList = document.createElement("ul"); 
+						div.appendChild(resultList); 
+												
+						// Renders list for tries per question. 
+						guessArray.forEach(function(guess, index){
+							
+							var questionResult = document.createElement("li"); 
+							resultList.appendChild(questionResult);
+							
+							if(guess.numberofwronganswers === 0){
+								questionResult.innerHTML = "Fråga " + guess.questionnr + ": " + "Du klarade frågan på första försöket."; 
+							}
+							else{
+								questionResult.innerHTML = "Fråga " + guess.questionnr + ": Du behövde " + (guess.numberofwronganswers + 1) + " försök.";
+							} 
+						}); 
 					}
-
-
 				}
-				else if(object.message == "Wrong answer! :(" && xhrAnswer.status === 400 && xhrAnswer.readyState === 4){
+				else if(object.message == "Wrong answer! :(" && xhrAnswer.status === 400){
 					numberOfWrongAnswers += 1; 
-					pFeedback.innerHTML = "Fel svar! Försök igen. Felaktiga svar: " + numberOfWrongAnswers; 
+					pFeedback.innerHTML = "Fel svar! Försök igen. Felaktiga svar på frågan: " + numberOfWrongAnswers; 
 					pFeedback.setAttribute("style", "color: red;");
+					// Clears input-field. 
 					inputAnswer.value = "";
 					getQuestion(); 
 				}
 			}
-			// // If bad request. 
-			// else if(xhr.readyState === 4 && xhr.status === 400){
-			// 	numberOfWrongAnswers += 1; 
-			// 	pFeedback.innerHTML = "Fel svar! Försök igen. Felaktiga svar: " + numberOfWrongAnswers; 
-			// 	pFeedback.setAttribute("style", "color: red;");
-			// 	inputAnswer.value = ""; 
-			// 	getQuestion();
-			// }
 		};
 
-		xhrAnswer.open("post", object.nextURL, true);
-		
-		xhrAnswer.setRequestHeader("Content-Type", "application/json"); 
-
+		xhrAnswer.open("post", object.nextURL, true);	
+		xhrAnswer.setRequestHeader("Content-Type", "application/json");
 		xhrAnswer.send(JSON.stringify(answerObject)); 
-
-		pFeedback.innerHTML = ""; 
 	}
 };
